c9579a36c1f6e08b498e89244c836ec060760e88
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2024-03-21 17:56:59 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
+import Navbar from './components/Navbar';
+import Products from './Pages/Products';
+import Recipes from './Pages/Recipes';
+import Comments from './Pages/Comments';
+import Users from './Pages/Users';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
-    <>
-      
-   
-    </>
-  )
+    <Router> {/* Wrap your entire application with Router */}
+      <div>
+        <Navbar />
+        <Routes> {/* Wrap your routes with Routes */}
+          <Route exact path="/" element={<Dashboard />} />
+          <Route exact path="/comments" element={<Comments />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav className='flex gap-12 justify-center items-center p-7'>
+            <div className="nav-logo">
+
+                <Link to={'/'}>
+                    <img src="" alt="Logo" />
+                </Link>
+            </div>
+
+            <div className="nav-url">
+                <ul className='flex gap-4'>
+                    <li><Link to={"/"}>Home</Link></li>
+                    <li><Link to={"/"}>About</Link></li>
+                    <li><Link to={"/"}>Contact</Link></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar

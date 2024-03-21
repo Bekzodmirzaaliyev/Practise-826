b2d105da3abd72fb01b5dc44af491b50af636bf1
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-03-21 17:58:03 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Rescipes from './Pages/Rescipes'
 
-function App() {
+const App = () => {
   return (
     <>
-      
-   
+      <Rescipes/>  
     </>
   )
 }
 
 export default App
+
---
+++ src/Pages/Rescipes.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { IoIosTime } from "react-icons/io";
+import { IoFastFood } from "react-icons/io5";
+import { IoEye } from "react-icons/io5";
+import { FaStar } from "react-icons/fa";
+import { MdOutlineDownloadDone } from "react-icons/md";
+
+const Rescipes = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/recipes")
+      .then((res) => res.json())
+      .then((data) => {
+        setRecipes(data.recipes);
+      });
+  }, []); // Empty dependency array to run effect only once
+
+  return (
+    <>
+      <div className="flex flex-wrap w-[90%] mx-auto justify-around gap-[30px]">
+        {recipes.map((recipe, id) => (
+          <div
+            key={id}
+            className="flex bg-slate-800 w-[300px] h-[540px] flex-col rounded-xl pb-3 shadow-xl shadow-slate-500 text-white "
+          >
+            <img
+              src={recipe.image}
+              alt=""
+              className="w-full max-h-[300px] rounded-t-lg"
+            />
+            <div className="flex flex-col text-center justify-center py-[10px] gap-5 px-[10px]">
+              <p className="font-bold text-[20px]">{recipe.name}</p>
+              <p className="flex gap-1 items-center">
+                <IoFastFood /> Calories: {recipe.caloriesPerServing}
+              </p>
+              <p className="flex gap-1 items-center">
+                <IoIosTime />
+                Cooking Time: {recipe.cookTimeMinutes}
+              </p>
+              <p className="flex gap-1 items-center">
+                <FaStar />
+                Rating: {recipe.rating}
+              </p>
+              <div className="flex justify-between">
+                <p className="flex gap-1 items-center">
+                  <IoEye />
+                  Viewed: {recipe.reviewCount}
+                </p>
+                <p className="flex gap-1 items-center">
+                  <MdOutlineDownloadDone /> Servings: {recipe.servings}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Rescipes;
